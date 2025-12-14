4497e59db8c69ae81374e78dd44eda95f4af686b
Wujue <wujue0115@gmail.com>
2025-12-14 20:41:54 +0800

    chore: release v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 34
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
